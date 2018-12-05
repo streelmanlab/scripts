614f67a441c034517d7362806ebfd37327066bf3
George Gruenhagen <ggruenhagen3@gatech.edu>
2018-12-05 10:33:17 -0500

    Making sure current version is updated

--- variant_calling/bam_prep.bash
+++ variant_calling/bam_prep.bash
@@ -18,8 +18,17 @@ usage () {
 		-M email of submitter"
 }
 
+check_for_help() {
+	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		usage;
+		exit 0;
+	fi
+}
+
 #Command-line options
 get_input() {
+	check_for_help "$1"
+	
 	bam=$1
 	picard=$2
 	shift
